57f916e00e3f7b01a1e397e9494e625110032950
Mrokoo <1874756925@qq.com>
2023-05-11 18:52:18 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using UserC;
+
+namespace 学习Csharp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("dsf");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            var p = new Person("bob22", 12);
+            p.SayHello();
+            var u1 = new User("bob", "112233@qq.com", "123344")
+            {
+                Coke = "Boke"
+            };
+            Console.WriteLine(u1.Name);
+        }
+    }
+
+    class Person : IWalk
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Person(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public void SayHello()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class Student : Person
+    {
+        public string Gender { get; set; }
+
+        public Student(string name, int id, string gender) : base(name, id)
+        {
+            Gender = gender;
+        }
+    }
+
+    interface IWalk
+    {
+        void SayHello();
+    }
+}
---
+++ User.cs
@@ -0,0 +1,30 @@
+﻿namespace UserC
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        private string? _Coke;
+        public string Coke
+        {
+            get
+            {
+
+                return _Coke ?? "nothing";
+            }
+            set
+            {
+                _Coke = value;
+            }
+        }
+    }
+}
